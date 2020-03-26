2357fa5d13a87a870ba3b5402783c652c4f1d6e4
Shamsijahan <imanli.shams@gmail.com>
2020-03-26 12:39:19 -0400

    First to branch

--- ProjectClass.java
+++ ProjectClass.java
@@ -1,10 +1,30 @@
 package git_project_package;
 
+import java.util.Scanner;
+
 public class ProjectClass {
 	
 	public static void main(String[] args) {
 		
 		System.out.println("Hello Shams from local");
+		
+		 Scanner scan = new Scanner(System.in);
+	        int num = scan.nextInt();
+	        fib(num);
+
+	    }
+
+	    public static void fib(int num){
+	        //WRITE YOUR CODE HERE
+	    	
+	    	int fibArr []= new int[num+2];
+	    	fibArr[0] = 0;
+	    	fibArr[1] = 1;
+	    	
+	    	for(int i = 2; i<=num; i++) {
+	    		fibArr[i]=fibArr[i-1]+fibArr[i-2];
+	    	} 
+	    	System.out.println(fibArr[num]);
 		
 	}
 
